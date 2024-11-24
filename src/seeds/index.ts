ce0e8c9ea9aa8b7d816f92d7f7ee224b0f0ae7ad
modifiedyoke <b.shurts@outlook.com>
2024-11-23 22:34:13 -0600

    added user seed data

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -1,5 +1,32 @@
-// import db from '../config/connection.js';
-// import { Thought, User } from '../models/index.js';
-// import cleanDB from './cleanDB.js';
+import db from '../config/connection.js';
+import { User } from '../models/index.js';
+import cleanDB from './cleanDB.js';
 
-console.log("No seed data created, because none exists");
+console.log("Seeding...");
+
+const users = [
+    {
+        "username": "theRealJohnWick",
+        "email": "noneof@your.biz",
+        "_id": "111111111111111111111111"
+    },
+    {
+        "username": "identityBourne",
+        "email": "jason@proton.net",
+        "_id": "222222222222222222222222"
+    },
+    {
+        "username": "jReacher",
+        "email": "inevercheck@this.email",
+        "_id": "333333333333333333333333"
+    }
+]
+
+try {
+    await db();
+    await cleanDB();
+    const newUsers = await User.insertMany(users);
+    console.log(newUsers);
+} catch (err) {
+    console.log(err);
+}
